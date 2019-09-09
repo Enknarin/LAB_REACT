35e0e3388fc59d26651a2235c4f0bf459dad8213
Enknarin <s5935512076@phuket.psu.ac.th>
2019-09-09 08:35:14 +0700

    edit App.js Classname Fit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Content from './Content';
+import CharacterCard from './CharacterCard';
 
 let x = 10
 class App extends Component{
 render(){
   return(
     <div className="App">
-      Hello {x}
+      Fit eiei eiei unreal got  {x}
       <Content/>
+      
     </div>
   );
 }
